ac4489bba1d6901e5161983362d4e21f52307ddd
Bobby Singh <bobbymarkbs@gmail.com>
2023-04-06 17:33:20 +0530

    added all files

---
+++ resources/views/home/header.blade.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html>
+   <head>
+      <!-- Basic -->
+      <meta charset="utf-8" />
+      <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+      <!-- Mobile Metas -->
+      <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+      <!-- Site Metas -->
+      <meta name="keywords" content="" />
+      <meta name="description" content="" />
+      <meta name="author" content="" />
+      <link rel="shortcut icon" href="home/images/favicon.png" type="">
+      <title>Famms - Fashion HTML Template</title>
+      <!-- bootstrap core css -->
+      <link rel="stylesheet" type="text/css" href="home/css/bootstrap.css" />
+      <!-- font awesome style -->
+      <link href="home/css/font-awesome.min.css" rel="stylesheet" />
+      <!-- Custom styles for this template -->
+      <link href="home/css/style.css" rel="stylesheet" />
+      <!-- responsive style -->
+      <link href="home/css/responsive.css" rel="stylesheet" />
+   </head>
+   <body>
+      <div class="hero_area">
+         <!-- header section strats -->
+         <header class="header_section">
+            <div class="container">
+               <nav class="navbar navbar-expand-lg custom_nav-container ">
+                  <a class="navbar-brand" href="{{route('home')}}"><img width="250" src="home/images/logo.png" alt="#" /></a>
+                  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                  <span class=""> </span>
+                  </button>
+                  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                     <ul class="navbar-nav">
+                        <li class="nav-item active">
+                           <a class="nav-link" href="{{route('home')}}">Home <span class="sr-only">(current)</span></a>
+                        </li>
+                       <li class="nav-item dropdown">
+                           <a class="nav-link dropdown-toggle" href="#" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="true"> <span class="nav-label">Pages <span class="caret"></span></a>
+                           <ul class="dropdown-menu">
+                              <li><a href="{{route('about')}}">About</a></li>
+                              <li><a href="{{route('testimonial')}}">Testimonial</a></li>
+                           </ul>
+                        </li>
+                        <li class="nav-item">
+                           <a class="nav-link" href="{{route('product')}}">Products</a>
+                        </li>
+                        <li class="nav-item">
+                           <a class="nav-link" href="{{route('blog')}}">Blog</a>
+                        </li>
+                        <li class="nav-item">
+                           <a class="nav-link" href="{{route('contact')}}">Contact</a>
+                        </li>
+                       
+                        <form class="nav-item">
+                           <button class="btn  my-2 my-sm-0 nav_search-btn" type="submit">
+                           <i class="fa fa-search" aria-hidden="true"></i>
+                           </button>
+                        </form>
+
+                        <li class="">
+                           <a class="btn btn-secondary" href="{{route('contact')}}">LOGIN</a>
+                        </li>
+
+                        <li class="nav-item">
+                           <a class="btn btn-primary" href="{{route('contact')}}">SIGNUP</a>
+                        </li>
+
+                     </ul>
+                  </div>
+               </nav>
+            </div>
+         </header>
+         <!-- end header section -->
---
+++ routes/web.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\controllers\HomeController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('home.homepage');
+})->name('home');
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified'
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
+
+Route::get('redirect',[HomeController::class,'redirect']);
+
+Route::get('about',function(){return view('home.about');})->name('about');
+Route::get('product',function(){return view('home.product');})->name('product');
+Route::get('contact',function(){return view('home.contact');})->name('contact');
+Route::get('blog',function(){return view('home.blog');})->name('blog');
+Route::get('testimonial',function(){return view('home.testimonial');})->name('testimonial');
